86908235bbc02972ebdd0c56a7c886db8f4b0517
Gonzalo Triviño <gonzalotcp@gmail.com>
2025-07-18 18:31:38 +0000

    first edit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['Mi perro ', 'Dayloc ','Mi gato ','Nef ','Lalo ']
+  let action = ['se comió ', 'buggeó ', 'robó ', 'orinó ', 'estrelló ']
+  let what = ['la tarea ', 'mi laptop ','los ahorros ','la mantita ','el cochazo de 4Geeks ']
+  let when = ['en el desayuno.','durante la clase de Java.','mientras dormía.','mientras programaba una web de impuestos.','tras su entrevista para Amazon.']
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyAray){
+  let max = anyAray.length - 1;
+  let min = 0;
+  let random = getRandomNumber (min, max);
+  return anyAray[random];
+} 
+
+document.getElementById("excuse").innerHTML = getRandom(who) + getRandom(action) + getRandom(what) + getRandom(when)
+
 };
